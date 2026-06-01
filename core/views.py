b4be804a3d541e9df4376bac755447ef318cb2f6
David Santiago Vargas Parra <dvargaspa@unal.edu.co>
2026-06-01 18:29:33 -0500

    Add HTTP method requirement to consult_videogames

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Videogame
 
+@require_http_methods(["GET"])
 def consult_videogames(request):
     videogames_db = Videogame.objects.all().values('id', 'name', 'genre', 'developer').order_by('id')
     videogames = [{"id": str(v['id']), "name": v['name'], "genre": v['genre'], "developer": v['developer']} for v in videogames_db]
